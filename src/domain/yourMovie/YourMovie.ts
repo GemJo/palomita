8e9accbb2305016010f2003f28e0ef0cf29a5f84
GemRed <gemjorba@gmail.com>
2022-08-31 19:37:38 +0200

    Added PersonalInfo into YourMovie

--- src/domain/yourMovie/YourMovie.ts
+++ src/domain/yourMovie/YourMovie.ts
@@ -2,6 +2,9 @@ import Movie from '@/domain/movie/Movie';
 import YourMovieId from '@/domain/yourMovie/YourMovieId';
 import YourMovieCreatedDate from '@/domain/yourMovie/YourMovieCreatedDate';
 import YourMovieLastUpdateDate from '@/domain/yourMovie/YourMovieLastUpdateDate';
+import PersonalInfoComment from '@/domain/yourMovie/personalInfo/PersonalInfoComment';
+import PersonalInfo from '@/domain/yourMovie/personalInfo/PersonalInfo';
+import PersonalInfoRating from '@/domain/yourMovie/personalInfo/PersonalInfoRating';
 
 export default class YourMovie {
   private movie: Movie;
@@ -12,14 +15,41 @@ export default class YourMovie {
 
   private lastUpdateDate: YourMovieLastUpdateDate;
 
+  private personalInfo: PersonalInfo;
+
   constructor(movie: Movie) {
     this.movie = movie;
     this.id = YourMovieId.generate();
     this.createdDate = YourMovieCreatedDate.generate();
     this.lastUpdateDate = YourMovieLastUpdateDate.generate();
+    this.personalInfo = new PersonalInfo();
   }
 
   public getId(): YourMovieId {
     return this.id;
+  }
+
+  public addComment(comment: PersonalInfoComment): void {
+    this.personalInfo.setComment(comment);
+  }
+
+  public comment(): PersonalInfoComment | null {
+    return this.personalInfo.getComment();
+  }
+
+  public addRating(rating: PersonalInfoRating): void {
+    this.personalInfo.setRating(rating);
+  }
+
+  public rating(): PersonalInfoRating | null {
+    return this.personalInfo.getRating();
+  }
+
+  public addViewed(viewed: boolean): void {
+    this.personalInfo.setViewed(viewed);
+  }
+
+  public viewed(): boolean {
+    return this.personalInfo.getViewed();
   }
 }
